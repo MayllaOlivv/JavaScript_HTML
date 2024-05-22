c23f2bf1499c2cdd8cf003220763f4d223923ef6
MayllaOliv <mayllamacetko13@gmail.com>
2024-05-22 20:50:34 -0300

    Atv JS

---
+++ JS/atv.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function() {
+    const images = document.querySelectorAll('img');
+    images.forEach(img => console.log(img));
+    });
+    
+    const infoParagraph = document.getElementById('info');
+    infoParagraph.textContent = 'Cingapura, uma cidade-estado localizada no sudeste da Ásia, é conhecida por sua eficiência, modernidade e diversidade cultural. Com uma população de cerca de 5,7 milhões de habitantes, é um dos centros financeiros mais importantes do mundo, destacando-se por seu ambiente de negócios favorável e infraestrutura de ponta. Cingapura impressiona com sua combinação de arranha-céus futuristas e áreas verdes meticulosamente planejadas, como os famosos Gardens by the Bay e a Marina Bay Sands. A cidade-estado também é reconhecida por seu rigor em manter a ordem e a limpeza, além de sua diversidade étnica que se reflete em uma vibrante cena gastronômica e cultural, onde coexistem influências chinesas, malaias, indianas e ocidentais.';
